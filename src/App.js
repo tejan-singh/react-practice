ad13b01883c833a19bc3f6634c263fd52200f207
tejan-singh <tstejan@gmail.com>
2021-08-05 18:15:09 +0530

    useState 3. remove all items and specific item

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import UseStateBasics from "./tutorial/1-useState/setup/2-useState-basics";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
 function App() {
   return (
     <div className="container">
-      <UseStateBasics />
+      <Setup />
     </div>
   );
 }
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import { data } from '../../../data';
 
+
+//! Notes - useState - remove item and all items
+/*
+- to remove all items, setState([])
+- to remove particular item, use filter to create new array of items except the item to remove and setState to new item list
+*/
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+
+  const clearList = function () {
+    setPeople([]);
+  };
+
+  const removeItem = function (id) {
+    let newPerson = people.filter((person) => {return(person.id !== id)})
+    setPeople(newPerson);
+  }
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div className="item" key={id}>
+            <h4>{name}</h4>
+            <button onClick={() => {removeItem(id)}}>remove</button>
+          </div>
+        );
+      })}
+
+      <button className="btn" onClick={clearList}>
+        Clear list
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;

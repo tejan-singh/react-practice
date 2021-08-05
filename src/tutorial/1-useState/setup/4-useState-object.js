37577c2ecd874280b4c82c0cfd333e797a698e37
tejan-singh <tstejan@gmail.com>
2021-08-05 18:52:14 +0530

    useState 4. update objects using SPREAD OPERATOR

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 
+//!Notes - useState - object
+/*
+- for setting new State of an object, use sperad operator (...) with object name, {propertyToUpdate: newValue} 
+*/
+
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message'
+  });
+
+  const setMessage = function(){
+    //set old state with spread operator and update the specified property only
+    setPerson({...person, message:'hello world'})
+  }
+
+  return (
+    <>
+      <h2>useState object example</h2>;
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button onClick={setMessage} className='btm'>change message</button>
+    </>
+  );
+  
 };
 
 export default UseStateObject;

769f907e65fb6cdceccc06a251151369ab8a04cb
tejan-singh <tstejan@gmail.com>
2021-08-08 23:53:52 +0530

    conditional returns with errors

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -16,19 +16,33 @@ const MultipleReturns = () => {
 
   useEffect(() => {
     fetch(url)
-      .then((response) => {return response.json()})
+      .then((response) => {
+        if(response.status >= 200 && response.status <= 299){
+          return response.json()
+        }else{
+          setIsLoading(false);
+          setIsError(true);
+          // send user the exact error message manually in the console in case if url is wrong
+          throw new Error(response.statusText)
+        }
+      })
       .then((user) => {
         const { login } = user;
         setUser(login);
         setIsLoading(false);
       })
       .catch((err) => {
+        // will only show network errors
         console.log(err);
       });
   }, []);
 
   if (isLoading) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>Error</h2>;
   }
 
   return <h2>{user}</h2>;

7431ef0fb1dd3aab0926230d1f67a0501f22d4fc
tejan-singh <tstejan@gmail.com>
2021-08-08 17:26:27 +0530

    useEffect basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
-// cleanup function
-// second parameter
+import React, { useState, useEffect } from "react";
+
+//! Notes - useEffect
+/* 
+- by default runs after every re-render.
+  - everytime state changes, page re renders and each time useEffect runs. useState evokes useEffect.
+  - use when you want to change/set values (sideeffects) //! outside your component
+    - used for 
+      - fetching data
+      - listening to events
+      - signing up for subscriptions
+- it takes a callback function as arguments
+- //! hooks cannot be called conditionally but You can call your function conditionally inside hooks/
+*/
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  useEffect(() => {
+    console.log("call useEffect");
+    if(value >= 1){
+      document.title = `New message (${value})`
+    }
+  });
+
+  const [value, setValue] = useState(0)
+
+  return (
+    <>
+      <h2>{value}</h2>;
+      <button className="btn" onClick={()=>{setValue(value + 1)}}>Click me</button>
+    </>
+  );
 };
 
 export default UseEffectBasics;

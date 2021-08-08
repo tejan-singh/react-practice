9d07c85906f9811a3bf516717413ef4112ae0678
tejan-singh <tstejan@gmail.com>
2021-08-08 19:35:29 +0530

    useEffect cleanup function

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,43 @@
 import React, { useState, useEffect } from 'react';
 
-// cleanup function
-// second argument
+//! notes - useEffect - cleanup function
+/*
+- used when we are conditionally rendering components
+- page renders -> useState runs -> useEffect runs
+- the no. of times page renders, the same will run the eventListeners no of times which will create problems.
+  - in order to avoid evoking eventlisteners again and again, you need to remove eventlistener once it is called, by using CLEANUP FUNCTION.
+- page re-render -> cleanup runs -> useEffect runs.
+- its good practice to set cleanup function when you set a event listener.
+*/
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+
+  const [size, setSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(()=>{
+    console.log('3. useEffect ran')
+
+    //takes only a callback function as argument
+    window.addEventListener('resize', checkSize)
+
+    //CLEANUP FUNCTION USAGE - returns a callback
+    return () => {
+      console.log('2. cleanup ran')
+      //removes the eventlistener and calls the callback function
+      window.removeEventListener('resize', checkSize)
+    }
+  })
+  
+  console.log('1. render page')
+
+  return <>
+    <h1>Window</h1>
+    <h2>{size}</h2>
+  </>;
 };
 
 export default UseEffectCleanup;

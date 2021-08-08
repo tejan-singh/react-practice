4002eb7cbcea82d6b8df2e233922414c1d6f1c40
tejan-singh <tstejan@gmail.com>
2021-08-08 22:59:37 +0530

    conditional returns

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from 'react';
+
+//! Notes - conditional rendering multiple returns
+/*
+- In JS when you define more then one return statements, then only the first one will execute.
+- If you want to render multiple return statements, then you can do so conditionally.
+
+*/
+
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
+
+  const[loading, setLoading] = useState(true)
+
+  if (loading){
+    return <h2>Loading...</h2>;
+  }
+
   return <h2>multiple returns</h2>;
 };
 

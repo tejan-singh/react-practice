faa0a25fb830ab04c419c637f5284107730cbb06
Tejan Singh <50990883+tejan-singh@users.noreply.github.com>
2021-08-15 14:31:09 +0530

    form: render all data on screen

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-/* Notes - HANDLING FORM DATA - CONTROLLED INPUTS
+/* Notes - RENDERING FORM DATA - CONTROLLED INPUTS
 
 React
 - while using forms,
@@ -13,13 +13,40 @@ React
   - 2. update "setState(e.target.value)" for each field using onChange.
   - 3. set "value" attribute equal to the state value of each input field.
 note:- if you do not define "onChange" attribute,state will not update and input fields will not work.
+- to render values on page after input:-
+  - performed using handleSubmit()
 */
 const ControlledInputs = () => {
   const [firstNameValue, setFirstName] = useState('')
   const [emailValue, setEmail] = useState('')
+  const [people, setPeople] = useState([])
 
   const handleSubmit = function(e){
     e.preventDefault()
+    
+    const person = {
+      firstName: firstNameValue,
+      email: emailValue
+    }
+
+    // direclty setting people will result into storing a object
+    // setPeople(person)
+
+    //use callback to pass the object inside the array
+    // this will also work but will render only only item and will update on render.
+    // setPeople(()=>{
+    //   return [person]
+    // })
+
+    // to render all the items in arrary, render all previous items stored in array on each render using spread operator
+    setPeople((people)=>{
+      // render array with previous data + new data
+      return [...people,person]
+    })
+
+    //you need to set the input fields to empty after items are rendered.
+    setFirstName('')
+    setEmail('')
 
   }
 
@@ -43,8 +70,19 @@ const ControlledInputs = () => {
       <input type='text' id='email' name='email' onChange={handleEmailChange} value={emailValue}></input>
       </div>
 
-      <button type='submit'>Submit</button>
+      <button type='submit'>Add person</button>
     </form>
+
+    
+    { people.map((eachPerson)=>{
+      
+      return <div className='item'>
+      <h4>{eachPerson.firstName}</h4>
+      <p>{eachPerson.email}</p>
+    </div>
+    })
+    }
+
   </article>
 }
 export default ControlledInputs;

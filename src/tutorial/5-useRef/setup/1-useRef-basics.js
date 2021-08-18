ae90272bbc8137704797abc02c6d054842c9e86f
Tejan Singh <50990883+tejan-singh@users.noreply.github.com>
2021-08-19 02:39:26 +0530

    initiated basic codebase

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,38 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from "react";
 
-// preserves value
+// preserves value between re-renders
 // DOES NOT trigger re-render
-// target DOM nodes/elements
+//use to  target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    console.log(refContainer.current);
+    console.log(refContainer.current.value);
+    console.log(divContainer.current)
+  };
+
+  useEffect(()=>{
+    // no need to set dependency array as page does not re-render with useRef.
+    //sets the cursor by default set on the input field when page renders.
+    refContainer.current.focus()
+  })
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer} />
+          <button type="submit">submit</button>
+        </div>
+      </form>
+
+      <div ref={divContainer}>Hello World</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -4,7 +4,40 @@ import { data } from '../../../data';
 // reducer function
 
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [name, setName] = useState('')
+  const [people, setPeople] = useState(data)
+  const [showModel, setShowModel] = useState(false)
+
+  const handleSubmit = function(e){
+    e.preventDefault()
+    if(name){
+      setShowModel(true)
+      setPeople([...people, {name:name}])
+      setName('')
+    }else{
+      setShowModel(false)
+    }
+  }
+
+  const handleChange = function(e){
+    setName(e.target.value)
+  }
+
+  return <>
+    {showModel && <Modal/>}
+    <form className='form' onSubmit={handleSubmit}>
+      <input type='text' onChange={handleChange} value={name}/>
+      <button className='btn' type='submit'>Add</button>
+    </form>
+    <div>
+      {people.map((person)=>{
+        
+        return(  
+          <p>{person.name}</p>
+        )
+      })}
+    </div>
+  </>;
 };
 
 export default Index;

d96577ba1eb5712a02279ad59297c8dd2230fc08
Tejan Singh <50990883+tejan-singh@users.noreply.github.com>
2021-08-22 01:53:16 +0530

    PropTypes: using AND OR operators to define default prop value

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -6,13 +6,20 @@ import defaultImage from '../../../assets/default-image.jpeg';
 //Notes - PropTypes
 /*
 - it is used to define default values to props in case value are undefined when fetching from APIs.
+- sometimes these default value does not render properly when fetched value and default value are of different type.
+- in our case, value for image is image.url(an object) and default value is defaultImage(a file). So default props are not rendered properly.
+- To solve this issue we can use && / || Operators
+- Note: in this case too, OR operator does not work direclty. JS does not recognise it. So we use AND Operator to return url when only when there is an image.
 */
 
 const Product = ({name, image, price}) => {
+  // means return image.url only when image prop is true or is defined
+  const imgUrl = image && image.url // Fix for rendering defaultImage
+
   return <article className='product'>
-    <p>{name}</p>
-    <img src={image.url} alt="default-img"/>
-    <p>{price}</p>
+    <p>{name || 'sample'}</p>
+    <img src={imgUrl || defaultImage} alt="default-img"/>
+    <p>{price || 3.99}</p>
   </article>;
 };
 
@@ -25,10 +32,10 @@ Product.propTypes = {
 }
 
 // 2. set default prop value for each prop
-Product.defaultProps = {
-  name: 'sample',
-  image: defaultImage,
-  price: 0
-}
+// Product.defaultProps = {
+//   name: 'sample',
+//   image: defaultImage,
+//   price: 0
+// }
 
 export default Product;

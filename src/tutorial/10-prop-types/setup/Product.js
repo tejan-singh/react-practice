7089ef36634ab365c97b5a508a9911fe606b0b90
Tejan Singh <50990883+tejan-singh@users.noreply.github.com>
2021-08-22 01:38:52 +0530

    PropTypes: using propTypes method for default prop value

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,34 @@
 import React from 'react';
+//import PropTypes
+import PropTypes from 'prop-types';
+import defaultImage from '../../../assets/default-image.jpeg';
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+//Notes - PropTypes
+/*
+- it is used to define default values to props in case value are undefined when fetching from APIs.
+*/
+
+const Product = ({name, image, price}) => {
+  return <article className='product'>
+    <p>{name}</p>
+    <img src={image.url} alt="default-img"/>
+    <p>{price}</p>
+  </article>;
 };
+
+// 1. set proptype for each prop
+Product.propTypes = {
+  //Proptypes.(typeofprop).(is required or not)
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+}
+
+// 2. set default prop value for each prop
+Product.defaultProps = {
+  name: 'sample',
+  image: defaultImage,
+  price: 0
+}
 
 export default Product;

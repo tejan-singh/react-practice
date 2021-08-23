d5ca93773f2f434d26c8809f675016d3152802f3
Tejan Singh <50990883+tejan-singh@users.noreply.github.com>
2021-08-23 12:51:42 +0530

    react-router: display dynamic pages using useParams() hook and find() method

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -2,9 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { data } from '../../../data';
 import { Link, useParams } from 'react-router-dom';
 const Person = () => {
+  // id from URL params can be accessed using useParams() but its type will be STRING
+  console.log(useParams())
+
+  const {id} = useParams()
+  const [name, setName] = useState('Default name')
+
+  //find is used to search for particular item inside array of objects. It will return an object.
+  // id as it is a STRING value need to be converted into interger so use parseInt(id)
+  const result = data.find((EachPersonDataObj) => {return EachPersonDataObj.id === parseInt(id)})
+
+  useEffect( ()=>{
+    setName(result.name)
+  },[])
+
   return (
     <div>
-      <h2>person</h2>
+      <h2>{name}</h2>
     </div>
   );
 };

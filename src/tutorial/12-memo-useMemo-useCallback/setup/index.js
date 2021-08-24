7974c085c532a283c36547681e07f96659b93e14
Tejan Singh <50990883+tejan-singh@users.noreply.github.com>
2021-08-24 19:52:02 +0530

    useCallBack(): avoid re-render of a nested component using useCallback

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 
-//Notes - React.memo()
+//Notes - useCallback()
 /*
-- if a component's prop value are same between re-render, then on state change, the components will not re-render
+- use to re-rendering of component only when certain value changes.
+- here we have 2 states - count, cart. 
+  - when either of those changes, page re-render.
+  - we want to re-render count component when value of count changes and stop re-rendering of cart component.
+  - which is done using useCallback() by specifying condition to re-render the cort component only when cart component value changes.
+  - cart component will not re-render when value for count changes.
+  - note: component re-render when prop value changes. When we were passing addToCart component to BigList component inside Index component, it is re-created everytime when count value changes and react thinks addToCart's value is changed.
 */
 
 
@@ -16,6 +22,12 @@ const url = 'https://course-api.com/javascript-store-products'
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
+  const [cart, setCart] = useState(0)
+  
+  //page will re-render only when cart value is changed. If any other value is changed, page will not re-render.
+  const addToCart = useCallback(function(){
+    setCart(cart + 1)
+  },[cart])
 
   return (
     <>
@@ -23,12 +35,17 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+
+      <h1 style={{marginTop:"3rem"}}>Cart : {cart}</h1>
+
+      <BigList products={products} addToCart={addToCart}/>
     </>
   )
 }
 
-const BigList = React.memo(({ products }) => {
+
+
+const BigList = React.memo(({ products, addToCart}) => {
   useEffect(()=>{
     console.log('Big list called')
   })
@@ -36,13 +53,13 @@ const BigList = React.memo(({ products }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return <SingleProduct key={product.id} {...product} addToCart={addToCart}></SingleProduct>
       })}
     </section>
   )
 })
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(()=>{
     console.log('Small list called')
   })
@@ -56,6 +73,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   )
 }
